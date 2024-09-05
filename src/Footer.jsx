16b50971f79f382877e797b1b5d830716fdecb3c
pivnati <natanmuleta911@gmail.com>
2024-09-04 19:58:35 -0700

    creating the footer

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-full bg-gray-800 p-4 text-white text-center">
+      <p>© 2024 Chat App</p>
+    </footer>
+  );
+};
+
+export default Footer;
