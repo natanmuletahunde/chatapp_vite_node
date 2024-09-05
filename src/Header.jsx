df92d0a68c1cde9030e439887b5b9c819348e468
pivnati <natanmuleta911@gmail.com>
2024-09-04 19:57:55 -0700

    creating the header

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="w-full bg-blue-500 p-4 text-white text-center">
+      <h1 className="text-3xl">Welcome to Our Chat App</h1>
+    </header>
+  );
+};
+
+export default Header;
